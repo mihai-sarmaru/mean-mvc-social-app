69098f28c1ebd2c31976fc2ec9deb866e524a12f
Mihai Sarmaru <mihai.sarmaru@gmail.com>
2020-05-12 12:40:55 +0300

    Start simple express app

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+// Require packages
 const express = require("express");
+
+// Express app
+const app = express();
+
+// Home page GET route
+app.get("/", (req, res) => {
+    res.send("Welcome to our new app");
+});
+
+// Listen on port 3000
+app.listen(3000);
