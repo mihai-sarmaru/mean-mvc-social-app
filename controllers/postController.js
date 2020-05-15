af710af61baf0227631998b5b839cdb93dd9f4b2
Mihai Sarmaru <mihai.sarmaru@gmail.com>
2020-05-15 13:57:07 +0300

    Include header section in EJS templates

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,3 +1,4 @@
+// Create post method
 exports.viewCreateScreen = function(req, res) {
-    res.render("create-post");
+    res.render("create-post", {username: req.session.user.username, avatar: req.session.user.avatar});
 }
