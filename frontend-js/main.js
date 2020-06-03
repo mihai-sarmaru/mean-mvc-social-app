6c937c4cb743a5f1fb59b681772c6ddb4a6c5f54
Mihai Sarmaru <mihai.sarmaru@gmail.com>
2020-06-03 14:30:34 +0300

    Add Search class with constructor and import it to main.js

---
+++ frontend-js/main.js
@@ -0,0 +1,5 @@
+// Import
+import Search from "./modules/search";
+
+// Create new instance of Search JS
+new Search();
---
+++ frontend-js/modules/search.js
@@ -0,0 +1,6 @@
+// Export Search class with constructor
+export default class Search {
+    constructor() {
+        alert("Search JS is executed");
+    }
+}
