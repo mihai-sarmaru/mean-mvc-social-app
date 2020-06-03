5168ee91f776c3745062e1ef6c3c9388f2241acf
Mihai Sarmaru <mihai.sarmaru@gmail.com>
2020-06-03 14:58:33 +0300

    Respond to search icon click event

--- frontend-js/modules/search.js
+++ frontend-js/modules/search.js
@@ -2,10 +2,24 @@
 export default class Search {
     // 1. Constructor - prop for selecting DOM elements
     constructor() {
-        
+        // Properties
+        this.headerSearchIcon = document.querySelector(".header-search-icon");
+
+        // Call the events method
+        this.events();
     }
 
-    // 2. Events
+    // 2. Events method
+    events() {
+        this.headerSearchIcon.addEventListener("click", (e) => {
+            // Prevent A default click event
+            e.preventDefault();
+            this.openOverlay();
+        });
+    }
 
     // 3. Methods
+    openOverlay() {
+        alert("Open overlay executed.");
+    }
 }
