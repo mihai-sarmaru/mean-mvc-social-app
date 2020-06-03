4e1db7fe65687742e7b4eb874bd007aab99c2cca
Mihai Sarmaru <mihai.sarmaru@gmail.com>
2020-06-03 15:21:32 +0300

    Create new instance of Search JS if user is logged in

--- frontend-js/main.js
+++ frontend-js/main.js
@@ -1,5 +1,7 @@
 // Import
 import Search from "./modules/search";
 
-// Create new instance of Search JS
-new Search();
+// Create new instance of Search JS if user is logged in - search icon exists
+if (document.querySelector(".header-search-icon")) {
+    new Search();
+}
