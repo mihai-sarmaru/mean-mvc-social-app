44855a8c0fda8ed4f130a428b2b57f7ef4938100
Mihai Sarmaru <mihai.sarmaru@gmail.com>
2020-06-12 15:47:49 +0300

    Add chat boilerplate code - chat class, call in main.js

--- frontend-js/main.js
+++ frontend-js/main.js
@@ -1,7 +1,9 @@
 // Import
 import Search from "./modules/search";
+import Chat from "./modules/chat";
 
 // Create new instance of Search JS if user is logged in - search icon exists
-if (document.querySelector(".header-search-icon")) {
-    new Search();
-}
+if (document.querySelector(".header-search-icon")) {new Search();}
+
+// Create new instance of Chat JS if wrapper DIV is found
+if (document.querySelector("#chat-wrapper")) {new Chat();}
---
+++ frontend-js/modules/chat.js
@@ -0,0 +1,15 @@
+// Export Chat class
+export default class Chat {
+    // Constructor
+    constructor() {
+        alert("Char feature is loading...");
+        this.events();
+    }
+
+    // Events
+    events() {
+
+    }
+
+    // Methods
+}
